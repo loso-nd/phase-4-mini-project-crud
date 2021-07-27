7ee19853208e729dff588882faa6b40ad26925d3
Nahum <losond8@gmail.com>
2021-07-27 13:43:45 -0700

    Generate an API that follows REST conventions. Handle full CRUD with one model.

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,32 @@
+class SpicesController < ApplicationController
+    #GET /spice
+    def index 
+        spices = Spice.all
+        render json: spices, except: [:created_at, :updated_at]
+    end
+
+    #POST /spice
+    def create 
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    #PATCH /spice/:id
+    def update 
+        spice = Spice.find_by(id: params[:id])
+        spice.update(spice_params)
+        render json: spice, status: :created
+    end
+
+    #DELETE /spice/:ID
+    def destroy 
+        spice = Spice.find_by(id: params[:id])
+        spice.destroy
+        head :no_content
+    end
+
+    private
+    def spice_params 
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+end
