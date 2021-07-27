b9d36ab8461bf71b13808ebe204e25b7cacb91fa
Nahum <losond8@gmail.com>
2021-07-27 16:37:38 -0700

    Server working now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :spices, only: [:index, :create, :update, :destroy]
-  #spelling error in resources, colon after only.
+  # resources :spices
 end
